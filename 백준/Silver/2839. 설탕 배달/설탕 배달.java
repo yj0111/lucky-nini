6d79a69b181382e40f32726d724a39a3327b7e22
Youjung <118320449+yj0111@users.noreply.github.com>
2024-10-23 15:12:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
 
-        int count = 0;
-
-        //일단 5그람을 최대한 많이 쓰자
-        while(N % 5 != 0 && N >=0){
-            N -= 3;
-            count++;
+        if (N < 3) {
+            System.out.println(-1);
+            return;
         }
 
-        if(N<0){
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, 9999);
+
+        dp[3] = 1;
+        if (N >= 5) dp[5] = 1;
+
+        for (int i = 6; i <= N; i++) {
+            dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+        }
+
+        if (dp[N] >= 9999) {
             System.out.println(-1);
-        }else{
-            count += N/5;
-            System.out.println(count);
+        } else {
+            System.out.println(dp[N]);
         }
     }
 }
