5dea9f9206bbe058316772caf345b1c8126fc5f5
Youjung <118320449+yj0111@users.noreply.github.com>
2024-10-24 22:47:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.36 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static boolean[][] visited;
+    static int[][] maps;
+    
+    public int solution(int[][] maps) {
+        this.maps = maps;
+        visited = new boolean[maps.length][maps[0].length];
+        int answer = bfs(0,0);//시작위치
+        return answer;
+    }
+    
+    static int bfs(int a, int b){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{a,b,1});
+        visited[a][b] = true;
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            int count = now[2];//거리
+            
+            //도착했어
+            if(x == maps.length-1 && y == maps[0].length-1){
+                return count;
+            }
+            
+            //4방향 탐색
+            for(int i = 0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && !visited[nx][ny] && maps[nx][ny] == 1){
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx,ny,count+1});
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
