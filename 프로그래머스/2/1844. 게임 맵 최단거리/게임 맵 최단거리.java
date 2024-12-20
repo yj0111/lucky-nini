913bd7e667d4e81792af5dff4f3cb23a9ea7e0b8
Youjung <118320449+yj0111@users.noreply.github.com>
2024-12-21 02:51:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.37 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -45,3 +45,54 @@ class Solution {
         
     }
 }
+
+//DFS로 풀어볼라했는데 > 시간 초과  ㅜ 그냥 BFS로 풀자 
+// import java.io.*;
+// import java.util.*;
+
+// class Solution {
+//     static int[] dx = {-1, 1, 0, 0};
+//     static int[] dy = {0, 0, -1, 1};
+//     static boolean[][] visited;
+//     static int[][] maps;
+//     static int min;
+
+//     public int solution(int[][] maps) {
+//         this.maps = maps;
+//         visited = new boolean[maps.length][maps[0].length];
+//         min = Integer.MAX_VALUE;
+
+//         // 시작위치!
+//         dfs(0, 0, 1);
+
+//         if(min == Integer.MAX_VALUE){
+//             return -1; 
+//         }
+        
+//         return min;
+//     }
+
+//     static void dfs(int x, int y, int count) {
+//         if (x < 0 || x >= maps.length || y < 0 || y >= maps[0].length 
+//             || visited[x][y] || maps[x][y] == 0) {
+//             return;
+//         }
+
+//         if (count >= min) {
+//             return;
+//         }
+        
+//         if (x == maps.length - 1 && y == maps[0].length - 1) {
+//             min = Math.min(min, count);
+//             return;
+//         }
+//             visited[x][y] = true;
+//             for (int i = 0; i < 4; i++) {
+//                 int nx = x + dx[i];
+//                 int ny = y + dy[i];
+//                 dfs(nx, ny, count + 1);
+//             }
+//             visited[x][y] = false;
+//     }
+// }
+
