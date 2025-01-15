3279bfa47de5e2a623a8cc550581b7e41cf75b1c
Youjung <118320449+yj0111@users.noreply.github.com>
2025-01-15 23:29:48 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 312 ms, Memory: 20048 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+      int N = sc.nextInt();
+    
+      for(int i = 1; i <= N; i++) {
+          for(int j = i; j < N; j++) {
+            System.out.print(" ");
+          }
+          for(int k = 0; k < i*2-1; k++) {
+            System.out.print("*");
+          }
+      System.out.println();
+    }
+  }
+}
